d6b7c78aad75dbb4edc6d26bbf192d5c43bcae5e
TomoyaWaniko <alligatorfree12@gmail.com>
2024-07-04 06:57:24 +0900

    update

--- 03-frontend/src/components/BookCheckoutPage/LatestReviews.tsx
+++ 03-frontend/src/components/BookCheckoutPage/LatestReviews.tsx
@@ -9,24 +9,16 @@ interface LatestReviewsProps {
   bookId: number | undefined;
 }
 
-const LatestReviews: FC<LatestReviewsProps> = (reviews, bookId) => {
+const LatestReviews: FC<LatestReviewsProps> = ({ reviews }) => {
   return (
     <div className={'flex justify-center space-y-3 p-3'}>
-      {reviews.reviews.length > 0 ? (
+      {reviews.length > 0 ? (
         <div className={'flex flex-row space-x-12'}>
           <h2 className={'text-4xl font-extralight'}>
             Latest <br /> Reviews:
           </h2>
-          {reviews.reviews.slice(0, 3).map((eachReview) => (
-            <Review
-              id={eachReview.id}
-              userEmail={eachReview.userEmail}
-              date={eachReview.date}
-              rating={eachReview.rating}
-              book_id={eachReview.book_id}
-              reviewDescription={eachReview.reviewDescription}
-              key={eachReview.id}
-            ></Review>
+          {reviews.slice(0, 3).map((eachReview) => (
+            <Review {...eachReview} key={eachReview.id} />
           ))}
           <div>
             <Button className={'default'} size={'lg'}>
--- 03-frontend/src/components/Utils/Review.tsx
+++ 03-frontend/src/components/Utils/Review.tsx
@@ -2,8 +2,8 @@ import { FC } from 'react';
 import ReviewModel from '../../models/ReviewModel.ts';
 import StarsReview from './StarsReview.tsx';
 
-const Review: FC<ReviewModel> = (review) => {
-  const newDate = new Date(review.date);
+const Review: FC<ReviewModel> = ({ userEmail, date, rating, reviewDescription }) => {
+  const newDate = new Date(date);
 
   const longMonth = newDate.toLocaleString('en-US', { month: 'long' });
   const dateDay = newDate.getDate();
@@ -14,12 +14,12 @@ const Review: FC<ReviewModel> = (review) => {
   return (
     <section className={'flex flex-row justify-center space-y-2 space-x-10'}>
       <div>
-        <h4 className={'font-extralight'}>{review.userEmail}</h4>
+        <h4 className={'font-extralight'}>{userEmail}</h4>
         <p className={'font-extralight'}>{dateRender}</p>
-        <p className={'text-xl font-semibold'}>{review.reviewDescription}</p>
+        <p className={'text-xl font-semibold'}>{reviewDescription}</p>
       </div>
       <div>
-        <StarsReview rating={review.rating} size={16} />
+        <StarsReview rating={rating} size={16} />
       </div>
     </section>
   );
